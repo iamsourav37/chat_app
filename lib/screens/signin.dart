4f9ad5b58f0dbf91e00d0f46aab79436830f099c
iamSourav37 <souravganguly108@gmail.com>
2020-12-02 19:17:19 -0800

    update signin screen

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/constants.dart';
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -7,6 +8,7 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,56 +23,114 @@ class _SignInState extends State<SignIn> {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
-                  fontSize: 21.0,
-                ),
-                decoration: kTextFieldInputDecoration.copyWith(
-                  hintText: 'email',
-                  prefixIcon: Icon(
-                    Icons.email,
-                    size: 25.0,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
+                    decoration: kTextFieldInputDecoration.copyWith(
+                      hintText: 'email',
+                      prefixIcon: Icon(
+                        Icons.email,
+                        size: 25.0,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 21.0,
-                ),
-                decoration: kTextFieldInputDecoration.copyWith(
-                  hintText: 'password',
-                  prefixIcon: Icon(
-                    Icons.vpn_key,
-                    size: 25.0,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
+                    decoration: kTextFieldInputDecoration.copyWith(
+                      hintText: 'password',
+                      prefixIcon: Icon(
+                        Icons.vpn_key,
+                        size: 25.0,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                alignment: Alignment.bottomRight,
-                child: GestureDetector(
-                  onTap: () {
-                    print("forget password...");
-                  },
-                  child: Text("Forget password?"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        print("forget password...");
+                      },
+                      child: Text("Forget password?"),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                  child: RoundedButton(
+                    onPressed: () {},
+                    btnColor: Colors.redAccent,
+                    btnText: "Signin",
+                    context: context,
+                    googleSignIn: false,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                  child: RoundedButton(
+                    onPressed: () {},
+                    btnText: "Signin with Google",
+                    btnColor: Colors.white,
+                    context: context,
+                    btnTextColor: Colors.black,
+                    googleSignIn: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Dont have an account ? ",
+                      style: TextStyle(
+                        fontSize: 17.0,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("Navigate to signup screen");
+                      },
+                      child: Text(
+                        "Register now",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class RoundedButton extends StatelessWidget {
+  final String btnText;
+  final Color btnColor, btnTextColor;
+  final Function onPressed;
+  final BuildContext context;
+  final bool googleSignIn;
+
+  RoundedButton(
+      {@required this.btnText,
+      @required this.btnColor,
+      @required this.onPressed,
+      @required this.context,
+      this.btnTextColor,
+      @required this.googleSignIn});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: RaisedButton(
+        onPressed: onPressed,
+        color: btnColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusDirectional.circular(30.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              googleSignIn
+                  ? Container(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child: Image(
+                        image: AssetImage('assets/images/google_logo.jpg'),
+                        height: 26.0,
+                      ),
+                    )
+                  : Container(),
+              Text(
+                btnText,
+                style: TextStyle(
+                  color: btnTextColor,
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
