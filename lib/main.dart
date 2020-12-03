c5cda8f97ad35f6195eb3b38c6c0545e2ecd2fa8
iamSourav37 <souravganguly108@gmail.com>
2020-12-02 19:23:32 -0800

    update signup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/screens/signin.dart';
+import 'package:chat_app/screens/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         accentColor: Colors.white,
       ),
-      home: SignIn(),
+      home: SignUp(),
     );
   }
 }
---
+++ lib/screens/signup.dart
@@ -0,0 +1,144 @@
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Signup",
+          style: TextStyle(
+            fontSize: 30.0,
+            letterSpacing: 1.8,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
+                    decoration: kTextFieldInputDecoration.copyWith(
+                      hintText: 'username',
+                      prefixIcon: Icon(
+                        Icons.person,
+                        size: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
+                    decoration: kTextFieldInputDecoration.copyWith(
+                      hintText: 'email',
+                      prefixIcon: Icon(
+                        Icons.email,
+                        size: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
+                    decoration: kTextFieldInputDecoration.copyWith(
+                      hintText: 'password',
+                      prefixIcon: Icon(
+                        Icons.vpn_key,
+                        size: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                  child: RoundedButton(
+                    onPressed: () {},
+                    btnColor: Colors.redAccent,
+                    btnText: "Signup",
+                    context: context,
+                    googleSignIn: false,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                  child: RoundedButton(
+                    onPressed: () {},
+                    btnText: "Signup with Google",
+                    btnColor: Colors.white,
+                    context: context,
+                    btnTextColor: Colors.black,
+                    googleSignIn: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Dont have an account ? ",
+                      style: TextStyle(
+                        fontSize: 17.0,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("Navigate to signup screen");
+                      },
+                      child: Text(
+                        "Register now",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
